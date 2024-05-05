31ffbde6ca5a0ada6b94739921a0a14bf5330dc5
nairabarseghyan <62842344+nairabarseghyan@users.noreply.github.com>
2024-05-06 01:06:43 +0400

    lag-llama final

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,4 +1,4 @@
-from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score, mean_absolute_percentage_error
 import numpy as np
 import pandas as pd
 
@@ -6,8 +6,7 @@ from config import DATA_PATH
 
 from gluonts.evaluation import Evaluator
 
-def mean_absolute_percentage_error(y_true, y_pred):
-    return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
+
 
 
 def evaluate_model(y_true_train, y_pred_train, y_true_val, y_pred_val, model_name):
@@ -62,100 +61,60 @@ def evaluate_model(y_true_train, y_pred_train, y_true_val, y_pred_val, model_nam
     
     
     
-def evaluate_lag_llama_model(tss_train, forecasts_train, tss_val, forecasts_val,model_name, prediction_type = "zs"):
+def evaluate_lag_llama_model(tss_val, forecasts_val, test_data, prediction_length, model_name):
     
     evaluator = Evaluator()
-    if prediction_type == "zs":
-        agg_metrics_train, ts_metrics_train = evaluator(iter(tss_train), iter(forecasts_train))
-        agg_metrics_val, ts_metrics_val = evaluator(iter(tss_val), iter(forecasts_val))
+
         
-        mae_train = mean_absolute_error(tss_train[0][0], forecasts_train[0].quantile(0.5))
-        mae_val = mean_absolute_error(tss_val[0][0], forecasts_val[0].quantile(0.5))
+    agg_metrics_val, ts_metrics_val = evaluator(iter(tss_val), iter(forecasts_val))
         
-        mape_train = agg_metrics_train["MAPE"]
-        mape_val = agg_metrics_val["MAPE"]
+
+    mae_val = agg_metrics_val["abs_error"] / prediction_length
         
-        rmse_train = agg_metrics_train["RMSE"]
-        rmse_val = agg_metrics_val["RMSE"]
+    
+    mape_val = agg_metrics_val["MAPE"]
         
-        r2_train = r2_score(tss_train[0][0], forecasts_train[0].quantile(0.5))
-        r2_val = r2_score(tss_val[0][0], forecasts_val[0].quantile(0.5))
+    
+    rmse_val = agg_metrics_val["RMSE"]
+        
+    
+    r2_val = r2_score(test_data[0]['target'], forecasts_val[0].quantile(0.5))
     
 
     
-        metrics = {
-            "model_name": model_name,
-            "mae_train": mae_train,
-            "mae_val": mae_val,
-            "mape_train": mape_train,
-            "mape_val": mape_val,
-            "rmse_train": rmse_train,
-            "rmse_val": rmse_val,
-            "r2_train": r2_train,
-            "r2_val": r2_val
-        }
+    metrics = {
+        "model_name": model_name,
+        
+        "mae_val": mae_val,
+        
+        "mape_val": mape_val,
+        
+        "rmse_val": rmse_val,
+        
+        "r2_val": r2_val
+    }
         
         # Convert dictionary to DataFrame
-        metrics_df = pd.DataFrame([metrics])
+    metrics_df = pd.DataFrame([metrics])
         
-        metrics_df.to_csv(f'{DATA_PATH}results/Models/{model_name}.csv',index = False)
+    metrics_df.to_csv(f'{DATA_PATH}results/Models/{model_name}.csv',index = False)
         
-        print(f"Results of model {model_name}: ")
+    print(f"Results of model {model_name}: ")
         # Print evaluation metrics
-        print("MAE (Mean Absolute Error):")
-        print(f"Train Set: {mae_train}")
-        print(f"Validation Set: {mae_val}\n")
-        
-        print("MAPE (Mean Absolute Percentage Error):")
-        print(f"Train Set: {mape_train}%")
-        print(f"Validation Set: {mape_val}%\n")
-        
-        print("RMSE (Root Mean Squared Error):")
-        print(f"Train Set: {rmse_train}")
-        print(f"Validation Set: {rmse_val}\n")
-        
-        print("R-squared (R2) Score:")
-        print(f"Train Set: {r2_train}")
-        print(f"Validation Set: {r2_val}\n")
-        
-    else: 
-
-        agg_metrics_val, ts_metrics_val = evaluator(iter(tss_val), iter(forecasts_val))
-        
-        
-        mae_val = mean_absolute_error(tss_val[0][0], forecasts_val[0].quantile(0.5))
-        
-        mape_val = agg_metrics_val["MAPE"]
+    print("MAE (Mean Absolute Error):")
     
-        rmse_val = agg_metrics_val["RMSE"]
+    print(f"Validation Set: {mae_val}\n")
+        
+    print("MAPE (Mean Absolute Percentage Error):")
     
-        r2_val = r2_score(tss_val[0][0], forecasts_val[0].quantile(0.5))
+    print(f"Validation Set: {mape_val}%\n")
+        
+    print("RMSE (Root Mean Squared Error):")
     
-
+    print(f"Validation Set: {rmse_val}\n")
+        
+    print("R-squared (R2) Score:")
     
-        metrics = {
-            "model_name": model_name,
-            "mae_val": mae_val,
-            "mape_val": mape_val,
-            "rmse_val": rmse_val,
-            "r2_val": r2_val
-        }
+    print(f"Validation Set: {r2_val}\n")
         
-        # Convert dictionary to DataFrame
-        metrics_df = pd.DataFrame([metrics])
-        
-        metrics_df.to_csv(f'{DATA_PATH}results/Models/{model_name}.csv',index = False)
-        
-        print(f"Results of model {model_name}: ")
-        # Print evaluation metrics
-        print("MAE (Mean Absolute Error):")
-        print(f"Validation Set: {mae_val}\n")
-        
-        print("MAPE (Mean Absolute Percentage Error):")
-        print(f"Validation Set: {mape_val}%\n")
-        
-        print("RMSE (Root Mean Squared Error):")
-        print(f"Validation Set: {rmse_val}\n")
-        
-        print("R-squared (R2) Score:")
-        print(f"Validation Set: {r2_val}\n")
+    
